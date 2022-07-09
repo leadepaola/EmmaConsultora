b326886c07c617fa20bbc413160dfc6b66cb217d
Leandro <leandrogdepaola@gmail.com>
2022-07-09 18:38:42 -0300

    update direccion de img

--- mensualidad/web_en_pausa.php
+++ mensualidad/web_en_pausa.php
@@ -58,7 +58,7 @@
 	
 	<div class="pausa_imagen_cont">
 	
-		<img class="pausa_imagen" src="../emmaconsultora/assets/images/LOGO-MODIFICADO2.png" alt="">
+		<img class="pausa_imagen" src="./assets/images/LOGO-MODIFICADO2.png" alt="">
 
 		<div class="pausa_txt" >Servidor pausado</div>
 
