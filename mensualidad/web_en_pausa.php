6b3154bdb9aaace49e58bf3b5552e597f09bdfa4
Leandro <leandrogdepaola@gmail.com>
2022-07-09 18:32:58 -0300

    update css

--- mensualidad/web_en_pausa.php
+++ mensualidad/web_en_pausa.php
@@ -3,56 +3,6 @@
 
 
 
-<style>
-	.pausa_section{
-
-		position: none; 
-
-		<?php 
-			//Variable declarada en mensualidad/verificar_dias.php
-			echo $pausa_position_img;
-
-		?>
-
-		z-index: 99999;
-
-		width: 100%;
-		height: 100%;
-
-		background-color: black;
-
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}	
-
-	.pausa_imagen_cont{
-		position: relative;
-		height: 25%;
-
-		top:-20px;
-	}
-
-	.pausa_imagen{
-		height: 100%;
-
-		/*background-color: red;*/
-	}
-
-	.pausa_txt{
-		color: white;
-		text-align: center;
-	}
-
-</style>
-
-
-
-
-
-
-
-
 
 <section class="pausa_section">
 	
