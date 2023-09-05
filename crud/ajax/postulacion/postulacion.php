77b34f079602a36aeb815e9d15b6736c48bcca71
Joel <83998903+joelgdeveloper@users.noreply.github.com>
2023-09-04 22:27:53 -0300

    Update postulacion.php

--- crud/ajax/postulacion/postulacion.php
+++ crud/ajax/postulacion/postulacion.php
@@ -1,7 +1,7 @@
 <?php
     include('../../../functions/connection.php');
     
-        if(isset($_FILES["archivo"]) && isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["telefono"]) && isset($_POST["mensaje"]) && isset($_POST["id_empleo"])){
+        if(isset($_FILES["archivo"]) && isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["telefono"]) && isset($_POST["mensaje"]) && isset($_POST["id_empleo"]) && isset($_POST["nombre_empleo"])){
             $archivo = trim(mysqli_real_escape_string($mysqli, $_FILES["archivo"]["name"]));
             $carpeta = 'archivos/';
             $id_postulacion= NULL;
@@ -10,8 +10,8 @@
             $email = trim(mysqli_real_escape_string($mysqli, $_POST["email"]));
             $telefono = trim(mysqli_real_escape_string($mysqli, $_POST["telefono"]));
             $mensaje = trim(mysqli_real_escape_string($mysqli, $_POST["mensaje"]));
-            // echo $id_empleo.' '.$nombre.' '.$email.' '.$telefono.' '.$mensaje;
-            // echo $_FILES["archivo"]['size'];
+            $nombre_empleo = trim(mysqli_real_escape_string($mysqli, $_POST["nombre_empleo"]));
+           
             $size = 1024 * 1024 * 10;
 
             if($_FILES["archivo"]['size'] < $size){
@@ -38,19 +38,26 @@
                 if(!file_exists($archivo)){
                     $sql = "INSERT INTO postulacion(id_postulacion, empleo_id, nombre, email, telefono, mensaje, curriculum) VALUES ('$id_postulacion','$id_empleo','$nombre','$email','$telefono','$mensaje','$archivo')";
 
-                    // mysqli_query($mysqli,$sql);
-
                     $resultado = $mysqli->query($sql);
 
                     if($resultado) {
                         echo "exito";
                         if(move_uploaded_file($_FILES['archivo']['tmp_name'], $archivo)){
-                            // echo "Archivo subido con exito";
-                            
-                            
                         }else{
                             echo "Error en el archivo";
                         }
+                        $to = "joelgdeveloper@gmail.com";
+                        $subject = "Nueva Postulación a Empleo: ". $nombre_empleo;
+                        
+                        $message = "Se ha recibido una nueva postulación para el empleo: $nombre_empleo. \n\n";
+                        $message .= "Nombre: $nombre \n";
+                        $message .= "Email: $email \n";
+                        $message .= "Teléfono: $telefono \n";
+                        $message .= "Mensaje: $mensaje \n\n\n";
+                        $message .= "Ingresa a la web para ver el CV de la persona";
+                        $headers = "From:" ."Consultas-EMMA";   
+
+                        mail($to,$subject,$message,$headers);
                     }else {
                         echo "Fallo en la postulacion";
                     }
