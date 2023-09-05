907088412dc9840ae558fc322b06d22e042c2c83
Joel <83998903+joelgdeveloper@users.noreply.github.com>
2023-09-04 22:40:28 -0300

    Update postulacion.php

--- crud/ajax/postulacion/postulacion.php
+++ crud/ajax/postulacion/postulacion.php
@@ -1,9 +1,9 @@
 <?php
     include('../../../functions/connection.php');
-    
+         var_dump($_POST);
+         die();
         if(isset($_FILES["archivo"]) && isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["telefono"]) && isset($_POST["mensaje"]) && isset($_POST["id_empleo"]) && isset($_POST["nombre_empleo"])){
-            var_dump($_POST);
-            die();
+           
             $archivo = trim(mysqli_real_escape_string($mysqli, $_FILES["archivo"]["name"]));
             $carpeta = 'archivos/';
             $id_postulacion= NULL;
