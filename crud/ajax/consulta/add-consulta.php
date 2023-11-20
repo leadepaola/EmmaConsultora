8adb5710418c9dabb0ecb0a478d2bc97f35b4ecf
joelgdeveloper <joelgdeveloper@gmail.com>
2023-11-20 11:20:34 -0300

    Prueba envios de emails

--- crud/ajax/consulta/add-consulta.php
+++ crud/ajax/consulta/add-consulta.php
@@ -1,5 +1,9 @@
 <?php
     include('../../../functions/connection.php');
+    require '../../../vendor/autoload.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
     if(isset($_POST['nombre']) && isset($_POST['email']) && isset($_POST['telefono']) && isset($_POST['mensaje'])){
         $id = NULL;
@@ -8,29 +12,50 @@
         $telefono = trim($_POST['telefono']);
         $mensaje = trim($_POST['mensaje']);
 
-        $to = "emmatalentos@gmail.com, ebabich@emmaconsultora.com, msotelo@emmaconsultora.com, info@emmaconsultora.com";
+        $to = "joelgdeveloper@gmail.com";
         $subject = "Tienen una consulta en la web - emmarecursoshumanos.com.ar";
-        
+
         $message = $mensaje."\n\n\n".
                    "Nombre: ".$nombre."\n".
                    "Email: ".$email."\n".
                    "Telefono: ".$telefono;
-        $headers = "From:" ."Consultas-EMMA";     
 
         $sql = "INSERT INTO contacto(id_contacto,nombre,email,telefono,mensaje) VALUES(?,?,?,?,?)";
-
         $stmt = $mysqli->prepare($sql);
-
         $stmt->bind_param("issss", $id,$nombre,$email,$telefono,$mensaje);
-       
+
         if($stmt->execute()){
-            echo 'exito';
-            mail($to,$subject,$message,$headers);
-        }else{
+            $mail = new PHPMailer(true);
+
+            try {
+                // Configuración del servidor
+                $mail->isSMTP();                                            
+                $mail->Host       = 'smtp.gmail.com';                     
+                $mail->SMTPAuth   = true;                                   
+                $mail->Username   = 'joelgdeveloper@gmail.com';                 
+                $mail->Password   = 'efpm dpjl ragc tapb';                            
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        
+                $mail->Port       = 587;                                    
+
+                // Destinatarios
+                $mail->setFrom('joeelrp14@gmail.com', 'Consultas-EMMA');
+                $mail->addAddress($to);
+
+                // Contenido
+                $mail->isHTML(false); // Poner en true si el cuerpo es HTML
+                $mail->Subject = $subject;
+                $mail->Body    = $message;
+
+                $mail->send();
+                echo 'exito';
+            } catch (Exception $e) {
+                echo "Fallo en la consulta";
+            }
+
+        } else {
             echo "Fallo en la consulta";
         }
-    }else{
-        echo "Error en el envio, revisa que los campos esten bien completados";
-    }
-        
+    } else {
+        echo "Error en el envío, revisa que los campos estén bien completados";
+    }        
 ?>
--- crud/ajax/postulacion/postulacion.php
+++ crud/ajax/postulacion/postulacion.php
@@ -1,69 +1,92 @@
 <?php
-    include('../../../functions/connection.php');
-    
-        if(isset($_FILES["archivo"]) && isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["telefono"]) && isset($_POST["mensaje"]) && isset($_POST["id_empleo"])){
-            $archivo = trim(mysqli_real_escape_string($mysqli, $_FILES["archivo"]["name"]));
-            $carpeta = 'archivos/';
-            $id_postulacion= NULL;
-            $id_empleo = trim(mysqli_real_escape_string($mysqli, $_POST["id_empleo"]));
-            $nombre = trim(mysqli_real_escape_string($mysqli, $_POST["nombre"]));
-            $email = trim(mysqli_real_escape_string($mysqli, $_POST["email"]));
-            $telefono = trim(mysqli_real_escape_string($mysqli, $_POST["telefono"]));
-            $mensaje = trim(mysqli_real_escape_string($mysqli, $_POST["mensaje"]));
-            // echo $id_empleo.' '.$nombre.' '.$email.' '.$telefono.' '.$mensaje;
-            // echo $_FILES["archivo"]['size'];
-            $size = 1024 * 1024 * 10;
+include('../../../functions/connection.php');
+require '../../../vendor/autoload.php';
 
-            if($_FILES["archivo"]['size'] < $size){
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-                $carpeta = 'archivos/';
-                $tipo = $_FILES['archivo']['type'];
-                $tipoNombre = $_FILES['archivo']['name'];
-                $info = new SplFileInfo($tipoNombre);
-                $infoExtension = $info->getExtension();
-                $fecha = date('Ymd-His');
+if (isset($_FILES["archivo"]) && isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["telefono"]) && isset($_POST["mensaje"]) && isset($_POST["id_empleo"])) {
+    $archivo = trim(mysqli_real_escape_string($mysqli, $_FILES["archivo"]["name"]));
+    $carpeta = 'archivos/';
+    $id_postulacion = NULL;
+    $id_empleo = trim(mysqli_real_escape_string($mysqli, $_POST["id_empleo"]));
+    $nombre = trim(mysqli_real_escape_string($mysqli, $_POST["nombre"]));
+    $email = trim(mysqli_real_escape_string($mysqli, $_POST["email"]));
+    $telefono = trim(mysqli_real_escape_string($mysqli, $_POST["telefono"]));
+    $mensaje = trim(mysqli_real_escape_string($mysqli, $_POST["mensaje"]));
+    $size = 1024 * 1024 * 10;
 
-                if($tipo == "application/pdf"){
-                    $archivoNombre = $carpeta.$fecha.".pdf";
-                }else if( $tipo == "text/plain"){
-                    $archivoNombre = $carpeta.$fecha.".txt";
-                }else if($infoExtension == "docx"){
-                    $archivoNombre = $carpeta.$fecha.".docx";
-                }else if($infoExtension == "doc"){
-                    $archivoNombre = $carpeta.$fecha.".doc";
-                }
+    if ($_FILES["archivo"]['size'] < $size) {
 
-                $archivo = $archivoNombre;
+        $tipo = $_FILES['archivo']['type'];
+        $tipoNombre = $_FILES['archivo']['name'];
+        $info = new SplFileInfo($tipoNombre);
+        $infoExtension = $info->getExtension();
+        $fecha = date('Ymd-His');
 
-                if(!file_exists($archivo)){
-                    $sql = "INSERT INTO postulacion(id_postulacion, empleo_id, nombre, email, telefono, mensaje, curriculum) VALUES ('$id_postulacion','$id_empleo','$nombre','$email','$telefono','$mensaje','$archivo')";
-
-                    // mysqli_query($mysqli,$sql);
-
-                    $resultado = $mysqli->query($sql);
-
-                    if($resultado) {
-                        echo "exito";
-                        if(move_uploaded_file($_FILES['archivo']['tmp_name'], $archivo)){
-                            // echo "Archivo subido con exito";
-                            
-                            
-                        }else{
-                            echo "Error en el archivo";
-                        }
-                    }else {
-                        echo "Fallo en la postulacion";
-                    }
-                    
-                }else{
-                    echo "El archivo ya existe";
-                }
-            }else{
-                echo "El archivo excede el tamaño permitido de 10mb";
-            }           
-            
-        }else{
-            echo "Error en el envio, revisa que los campos y el documento esten bien seleccionados";
+        if ($tipo == "application/pdf") {
+            $archivoNombre = $carpeta . $fecha . ".pdf";
+        } else if ($tipo == "text/plain") {
+            $archivoNombre = $carpeta . $fecha . ".txt";
+        } else if ($infoExtension == "docx") {
+            $archivoNombre = $carpeta . $fecha . ".docx";
+        } else if ($infoExtension == "doc") {
+            $archivoNombre = $carpeta . $fecha . ".doc";
         }
-    
+
+        $archivo = $archivoNombre;
+
+        if (!file_exists($archivo)) {
+            $sql = "INSERT INTO postulacion(id_postulacion, empleo_id, nombre, email, telefono, mensaje, curriculum) VALUES ('$id_postulacion','$id_empleo','$nombre','$email','$telefono','$mensaje','$archivo')";
+            $resultado = $mysqli->query($sql);
+
+            if ($resultado) {
+
+                if (move_uploaded_file($_FILES['archivo']['tmp_name'], $archivo)) {
+                    // Enviar el correo usando PHPMailer
+                    $mail = new PHPMailer(true);
+
+                    try {
+                        // Configuración del servidor
+                        $mail->isSMTP();                                            
+                        $mail->Host       = 'smtp.gmail.com';                     
+                        $mail->SMTPAuth   = true;                                   
+                        $mail->Username   = 'joelgdeveloper@gmail.com';                 
+                        $mail->Password   = 'efpm dpjl ragc tapb';                            
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        
+                        $mail->Port       = 587;    
+                        $to = 'joelgdeveloper@gmail.com';
+
+                        // Destinatarios
+                        $mail->setFrom('joeelrp14@gmail.com', 'Consultas-EMMA');
+                        $mail->addAddress($to);
+
+                        // Contenido
+                        $mail->isHTML(false);
+                        $mail->Subject = 'Nuevo postulante para el empleo ' . $id_empleo;
+                        $mail->Body = $mensaje . "\n\n\n" . "Nombre: " . $nombre . "\n" . "Email: " . $email . "\n" . "Telefono: " . $telefono;
+                        $mail->addAttachment($archivo);
+
+                        $mail->send();
+                        echo "exito";
+                    } catch (Exception $e) {
+                        echo "Error en el envío del correo: {$mail->ErrorInfo}";
+                    }
+                } else {
+                    echo "Error en el archivo";
+                }
+            } else {
+                echo "Fallo en la postulacion";
+            }
+
+        } else {
+            echo "El archivo ya existe";
+        }
+    } else {
+        echo "El archivo excede el tamaño permitido de 10mb";
+    }
+
+} else {
+    echo "Error en el envio, revisa que los campos y el documento esten bien seleccionados";
+}
 ?>
